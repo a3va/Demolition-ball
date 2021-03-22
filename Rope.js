9f5489b190cf1049e54987261fbb02df2e9d52fe
a3va <72570099+a3va@users.noreply.github.com>
2021-03-22 14:43:35 +0400

    Add files via upload

---
+++ Rope.js
@@ -0,0 +1,49 @@
+class Rope{
+    constructor(x, y, width, height, angle) {
+        var options = {
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.0
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        rectMode(CENTER);
+        rect(0, 0, this.width, this.height);
+        pop();
+      }
+}
+
+class Slingshot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 1,
+            length: 200
+        }
+        this.sling = Matter.Constraint.create(options);
+        this.pointB = pointB;
+        World.add(world, this.sling);
+    }
+
+    display(){
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(4);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+    
+    fly(){
+        this.sling.bodyA = null;
+    }
+}
